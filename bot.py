80fd458e1c65fe48d6b7b588bf3c6a712346d777
Danny Barbosa <132908376+DannyBarbosaBR@users.noreply.github.com>
2024-10-07 17:25:54 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 import json
 import datetime as dt
 
-TOKEN = 'YOUR BOTS TOKEN HERE'
+TOKEN = 'MTI5MjY0Mzc1MjIzNzczMTk1MA.G6ByvG.b4oG4yDn5rZfna0n8KhsYNrbUakZuMOpGE_i5o'
 client = commands.Bot(command_prefix='/')
 client.remove_command('help')
 
